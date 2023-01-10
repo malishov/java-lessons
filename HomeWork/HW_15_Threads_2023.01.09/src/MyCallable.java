7ae2d019173035c5e1b6142fb99479792d5f0082
malishov <alishov.murad831@gmail.com>
2023-01-10 16:53:05 +0400

    hw14 update

--- HomeWork/HW_15_Threads_2023.01.09/src/MyCallable.java
+++ HomeWork/HW_15_Threads_2023.01.09/src/MyCallable.java
@@ -1,17 +1,19 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class MyCallable implements Callable<Integer> {
-//
+
     public static String input = "data.txt";
-    public ExecutorService executorService = Executors.newFixedThreadPool(3);
-//
+    public ExecutorService executorService = Executors.newFixedThreadPool(4);
+
     @Override
     public Integer call() throws Exception {
 
-        int count = 0;
+        List<Integer> futures = new ArrayList<>();
 
         try {
 
@@ -24,17 +26,12 @@ public class MyCallable implements Callable<Integer> {
 
                 Callable<Integer> callable = () -> {
 
-                    Thread.sleep(5000);
                     System.out.println(Thread.currentThread().getName() + " " + tmpLine.split(" ").length);
-                    return tmpLine.split(" ").length; };
+                    return tmpLine.split(" ").length;
+                };
 
                 Future<Integer> future = executorService.submit(callable);
-
-//                FutureTask<Integer> future = new FutureTask<>(callable);
-//                new Thread(future).start();
-
-                count += future.get();
-
+                futures.add(future.get());
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -42,6 +39,6 @@ public class MyCallable implements Callable<Integer> {
             executorService.shutdown();
         }
 
-        return count;
+        return futures.stream().reduce(0, Integer::sum);
     }
 }
